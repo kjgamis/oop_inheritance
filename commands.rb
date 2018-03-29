d1c7ef8d78e79827302bf104ba95157d493548e0
Karen <kjgamis@live.ca>
2018-03-29 17:34:36 -0400

    clean up

---
+++ commands.rb
@@ -0,0 +1,15 @@
+person = Quotecollector.new
+person.memorize("Don't cry because it's over, smile because it happened.")
+person.memorize("Be yourself; everyone else is already taken.")
+person.memorize("You only live once, but if you do it right, once is enough.")
+person.memorize("In three words I can sum up everything I've learned about life: it goes on.")
+person.memorize("If you tell the truth, you don't have to remember anything.")
+
+
+# puts Quotecollector.phrases
+
+# puts Quotecollector.random
+person.travel_to("Italy")
+person.travel
+
+puts person.random
--- quotecollector.rb
+++ quotecollector.rb
@@ -9,33 +9,15 @@ class Quotecollector < Multilinguist
   end
 
   def translate(phrase)
-    "#{self.say_in_local_language(phrase)}"
+    say_in_local_language(phrase)
   end
 
   def self.phrases
     @@phrases
   end
 
-  def self.random
-    a = @@phrases.sample
+  def random
+    say_in_local_language(@@phrases.sample)
   end
 
 end
-
-person = Quotecollector.new
-person.memorize("Don't cry because it's over, smile because it happened.")
-person.memorize("Be yourself; everyone else is already taken.")
-person.memorize("You only live once, but if you do it right, once is enough.")
-person.memorize("In three words I can sum up everything I've learned about life: it goes on.")
-person.memorize("If you tell the truth, you don't have to remember anything.")
-
-
-# puts Quotecollector.phrases
-
-# puts Quotecollector.random
-person.travel_to("Italy")
-
-puts Quotecollector.random
-
-# puts Quotecollector.translate(Quotecollector.random)
-# puts person.translate('Hello there')
